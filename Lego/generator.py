ebe0698663d9b7f0054a6cd3d7f5258980e1f1c0
Protino <gurupadmamadapur@gmail.com>
2017-03-13 13:03:11 +0530

    Add aspect_ratios

--- Lego/generator.py
+++ Lego/generator.py
@@ -1,9 +1,18 @@
 import requests,ast
+
+#Aspect_ratio stuff
+from PIL import Image
+def calculate_aspect_ratio(image_file):
+    size = Image.open(image_file).size
+    return round(size[0]/size[1],2)
+
+
+
 '''
 data
 '''
 
-default_json=[{'author': 'Carl Sagan', 'id': '4', 'published_date': '2013-06-20T00:00:00.000Z'}, {'author': 'Edgar Allen Poe', 'id': '2', 'published_date': '2013-01-19T00:00:00.000Z'}, {'author': 'Ernest Hemingway', 'id': '11', 'published_date': '2013-06-19T00:00:00.000Z'}, {'author': 'Vincent Van Gogh', 'id': '3', 'published_date': '2013-05-10T00:00:00.000Z'}, {'author': 'Tom Brokaw', 'id': '1', 'published_date': '2013-05-02T00:00:00.000Z'}, {'author': 'Thomas Edison', 'id': '5', 'published_date': '2013-03-19T00:00:00.000Z'}, {'author': 'Plato', 'id': '7', 'published_date': '2013-01-16T00:00:00.000Z'}, {'author': 'Robin Williams', 'id': '8', 'published_date': '2013-01-15T00:00:00.000Z'}, {'author': 'Jacqueline Kennedy Onasis', 'id': '9', 'published_date': '2013-01-14T00:00:00.000Z'}, {'author': 'Pablo Picasso', 'id': '10', 'published_date': '2013-01-13T00:00:00.000Z'}, {'author': 'Margaret Thatcher', 'id': '6', 'published_date': '2013-01-12T00:00:00.000Z'}, {'author': 'Ernest Hemingway', 'id': '12', 'published_date': '2013-01-12T00:00:00.000Z'}, {'author': 'Ernest Hemingway', 'id': '13', 'published_date': '2013-01-12T00:00:00.000Z'}, {'author': 'Ernest Hemingway', 'id': '14', 'published_date': '2013-01-12T00:00:00.000Z'}, {'author': 'Ernest Hemingway', 'id': '15', 'published_date': '2013-01-12T00:00:00.000Z'}, {'author': 'Ernest Hemingway', 'id': '16', 'published_date': '2013-01-12T00:00:00.000Z'}, {'author': 'Ernest Hemingway', 'id': '17', 'published_date': '2013-01-12T00:00:00.000Z'}]
+default_json=[{"author": "Carl Sagan", "id": "4", "published_date": "2013-06-20T00:00:00.000Z"}, {"author": "Edgar Allen Poe", "id": "2", "published_date": "2013-01-19T00:00:00.000Z"}, {"author": "Ernest Hemingway", "id": "11", "published_date": "2013-06-19T00:00:00.000Z"}, {"author": "Vincent Van Gogh", "id": "3", "published_date": "2013-05-10T00:00:00.000Z"}, {"author": "Tom Brokaw", "id": "1", "published_date": "2013-05-02T00:00:00.000Z"}, {"author": "Thomas Edison", "id": "5", "published_date": "2013-03-19T00:00:00.000Z"}, {"author": "Plato", "id": "7", "published_date": "2013-01-16T00:00:00.000Z"}, {"author": "Robin Williams", "id": "8", "published_date": "2013-01-15T00:00:00.000Z"}, {"author": "Jacqueline Kennedy Onasis", "id": "9", "published_date": "2013-01-14T00:00:00.000Z"}, {"author": "Pablo Picasso", "id": "10", "published_date": "2013-01-13T00:00:00.000Z"}, {"author": "Margaret Thatcher", "id": "6", "published_date": "2013-01-12T00:00:00.000Z"}, {"author": "Ernest Hemingway", "id": "12", "published_date": "2013-01-12T00:00:00.000Z"}, {"author": "Ernest Hemingway", "id": "13", "published_date": "2013-01-12T00:00:00.000Z"}, {"author": "Ernest Hemingway", "id": "14", "published_date": "2013-01-12T00:00:00.000Z"}, {"author": "Ernest Hemingway", "id": "15", "published_date": "2013-01-12T00:00:00.000Z"}, {"author": "Ernest Hemingway", "id": "16", "published_date": "2013-01-12T00:00:00.000Z"}, {"author": "Ernest Hemingway", "id": "17", "published_date": "2013-01-12T00:00:00.000Z"}]
 
 trippy="""So, there was this DJ He was like, kicking off, I don't know what he was doing.But it was sick, man. Like, he was like, hands in the air, like, screaming out. So, like, this clown started covering us in silly string and we were all like, writhing around on the floor, at least I thought we were and then this cat walked in, you know, not like a cat, like a feline cat, like a real, like, you know, like, you know what I'm saying dawg? Like cats and dogs. It was raining, it wasn't raining, we were coding. I ate her, man. Not like eight, like nine. Like, I ate her she was fine, man. Like, you know, like, eatin' and sleepin' and ravin' repeatin', you know. There were people dancing I think, or maybe they were cops. I think they might of been cops. Well, anyway, like, I was just dancing and dancing, Oh, no, they were cops.
 <br><br><i>Shit.</i><br><br>And this cop just looked at me. And I don't know whether he was really saying it, but all he kept saying was<br><br><i>Eat, sleep, code, repeat,<br>Eat, sleep, code, repeat</i><br><br>Suddenly I think I'm on an interview<br>Suddenly I think I'm selling myself,<br>But I'm not<br>I'm just dreaming,<br> I'm just dreaming,<br>I'm just sleepin'<br>I'm just eatin'<br>I'm just codin'<br>I'm just repeatin'<br>And on, and on, and on, and on, and on, and on<br><br>I felt this thud, it was a bsod, like, boom clutch boom, man<br>Sorry dude, I thought you were a object<br>I went into this cubicle, and the guy was, like, you need something?<br>I'm like no, I'm just dancing to the hum of your cpu fan<br>He's like then get the out of my cubicle,<br>I'm like, I like it here, I like the lighting<br>Besides I like your assistant, she looks pretty hot<br>So I got her by the arm, and I took her outside, and I gave her to the homeless guy<br>He gave me all his google cloud credits<br>And all he kept saying was<br><i><br>Eat, sleep, code, repeat<br>Eat, sleep, code, repeat<br></i><br>'Til like John called me the next morning,<br>Dude, like, where were you last night?<br>I was like, I was there<br>He was like, oh, yeah<br>And then he was like, remember that tune they were playing?<br>I'm like, I don't remember anything, man<br>
@@ -30,7 +39,7 @@ base_url=("https://raw.githubusercontent.com/Protino/dump/master/Lego/","https:/
 
 photo_url=base_url+'images/'
 thumb_url=base_url+'thumbs/'
-data_url="https://raw.githubusercontent.com/Protino/dump/master/Lego/new_data.json"
+data_url="https://raw.githubusercontent.com/Protino/dump/master/Lego/data.json"
 
 if use_udacity_images:
     names = ['p004.jpg', 'p002.jpg', 'p011.jpg', 'p003.jpg', 'p001.jpg', 'p005.jpg', 'p007.jpg', 'p008.jpg', 'p009.jpg', 'p010.jpg', 'p006.jpg', 'p012.jpg', 'p013.jpg', 'p014.jpg', 'p015.jpg', 'p016.jpg', 'p017.jpg']
@@ -38,7 +47,7 @@ if use_udacity_images:
 else:
     #Following are the repository images, not the udacity ones. These are more beautiful but are around ~400 kb, making app slower to render
     names = ['audi.jpg', 'beach.jpg', 'bliss.jpg', 'coffee.jpg', 'green.jpg', 'hills.jpg', 'lake.jpg', 'white_house.jpg', 'random.jpg', 'sat.jpg', 'scape.jpg', 'shuttle.jpg', 'sky.jpg', 'solar_system.jpg', 'sunset.jpg', 'vegas.jpg', 'whatever.jpg', 'orange.jpg']
-    aspect_ratios = [-1]*len(names)
+    aspect_ratios = [calculate_aspect_ratio('images/'+x) for x in names]
 
 if use_udacity_content:
     titles=['Mysteries of the Universe Solved', 'A Flatiron State of Mind', 'An Empire State of Mind', '10 Tips for Hipster Tea Parties', 'My Story of Climbing a Mountain', 'How Fido Got His Bone Back', 'Why I Love Yellow', "Agriculturist's Weekly Update, Delivered Once Daily, 24/7", 'Brooklyn Sidewalks Anonymous', '3 Great Dessert Recipes for This Weekend', 'TV in Modern Beach Environments', 'What I Found While Swimming', 'Bourgeois Office Furniture', 'String Beans and What They Mean for You', "I Can't Get Enough Fantastic Sunsets", 'The Beauty That is Mount Pumpkinfoot', "Busy Streets Are Still Busy, Even If You Don't Want Them To Be"]
@@ -46,11 +55,10 @@ if use_udacity_content:
 else:
     titles=["Doctor Strange's Mystical Universe", "Dude's State of Mind", 'An Empire State of Mind', '10 Tips for Hipster Coffee Parties', 'How I Tried Climbing and Broke my Leg', 'PuG is Different than Pug', 'Why I Love Abstract Art', 'Lost In the Wilderness', 'Jumping From a Space Shuttle in 3 Easy Steps', '3 Great Desert Recipes that Martians Adore', 'Lost in the Wilderness - II', 'How I Broke my Back Again', 'Illuminati-on', "Research says Beans will be Extinct in 2050", "I Can't Get Enough of Fantastic Sunsets", 'How I Sold my Mansion for Free', 'Lost in the Wierdness']
     body=(trippy,psych,satan)
-    
-    
 
 photos=[photo_url+x for x in names]
 thumbs=[thumb_url+x for x in names]
+
 
 def construct_json():
     print ("Constructing json");
@@ -65,10 +73,10 @@ def construct_json():
 
 def save():
     print ("Saving json data");
-    str_json="["+','.join(list(map(str,default_json)))+"]"
-    with open('new_data.json','w')as f:
+    str_json="["+",".join(list(map(str,default_json)))+"]"
+    with open('data.json','w')as f:
         f.write(str_json)
-    with open('new_data.json','r') as f:
+    with open('data.json','r') as f:
         if f.read()!=str_json:
             raise Exception('Save failed!')
         else:
@@ -77,7 +85,7 @@ def save():
 def test_endpoint():
     response =  requests.get(data_url)
     remote_data=ast.literal_eval(response.text)
-    with open('new_data.json','r') as f:
+    with open('data.json','r') as f:
         local_data=ast.literal_eval(f.read())
     assert (len(remote_data)==len(local_data))
     for i in range(len(remote_data)):
@@ -91,9 +99,10 @@ def test_endpoint():
     print ("Test completed successfully" if local_data==remote_data else "Incorrect response")
 
 def main():
-    #construct_json();save()
+    construct_json()
+    save()
     #Make sure the changes are committed,pushed and about 30s are elapsed else test will fail
-    test_endpoint()
+    #test_endpoint()
     
 if __name__=='__main__':main()
     
