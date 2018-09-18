4a886287b3e687c5eee4537b9331b716c7c4a909
Gurupad Mamadapur <gurupad.mamadapur@airveda.com>
2018-09-18 17:19:05 +0530

    Add blog HTML files

---
+++ blog/blog.py
@@ -0,0 +1,187 @@
+BLOG_DATA = [
+    {
+        'name': 'Why We Should Measure Carbon Dioxide (CO2)?',
+        'link': 'http://www.airveda.com/blog/Why-We-Should-Measure-Carbon-Dioxide-CO2',
+        'mobile_link': '',
+        'date': 'August 14, 2018',
+        'author': 'Team Airveda',
+        'author_photo': 'http://www.airveda.com/resources/images/team_airveda.jpg',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'Carbon dioxide,Indoor air quality,Air quality,health,air quality monitor,ventilation',
+        'image': 'http://www.airveda.com/resources/images/understanding_co2.jpg',
+        'content': 'Carbon dioxide is a natural constituent of the air we breathe. Humans breathe in oxygen and release carbon dioxide, plants breathe in carbon dioxide and release oxygen. So why lately, are we hearing so much about carbon dioxide as an air pollutant?'
+    }, {
+        'name': 'Busting Myths Around Air Quality',
+        'link': 'http://www.airveda.com/blog/Busting-Myths-Around-Air-Quality',
+        'mobile_link': '',
+        'date': 'May 16, 2018',
+        'author': 'Team Airveda',
+        'author_photo': 'http://www.airveda.com/resources/images/team_airveda.jpg',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'air pollution,air quality monitor,air quality monitoring devices,PM2.5,PM10,air quality sensors,air quality myths,greenpeace',
+        'image': 'http://www.airveda.com/resources/images/busting_myths.jpg',
+        'content': 'WHO recently published their latest study which revealed that the 14 most polluted cities in the world are in India. It is clear that air pollution is not just a Delhi problem. It is also clear that the scale of the problem is huge and air pollution'
+    }, {
+        'name': 'Understanding Indoor Air Quality - Some Frequently Asked Questions',
+        'link': 'http://www.airveda.com/blog/Understanding-Indoor-Air-Quality',
+        'mobile_link': '',
+        'date': 'March 22, 2018',
+        'author': 'Team Airveda',
+        'author_photo': 'http://www.airveda.com/resources/images/team_airveda.jpg',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'air quality monitor, air quality monitoring devices,PM2.5,PM10,air quality sensors,air pollution,indoor air,questions,house plants,buy an air purifier,tips,buy humidifier,VOC,detergents',
+        'image': 'http://www.airveda.com/resources/images/air_quality_monitoring.jpg',
+        'content': 'We know when food is dirty or water is impure, and take every precaution before consuming them. But when it comes to air, we do not realize how polluted it is because we cannot see it. A person with normal activity level breathes in 2 gallons of air'
+    }, {
+        'name': 'Working Out With Asthma - Exercises That Are Safe For People With Asthma',
+        'link': 'http://www.airveda.com/blog/8-Exercises-That-People-With-Asthma-Can-Do-Daily',
+        'mobile_link': '',
+        'date': 'January 18, 2018',
+        'author': 'Team Airveda',
+        'author_photo': '',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'asthma,respiratory wellness,exercising for wellness,breathe well',
+        'image': 'http://www.airveda.com/resources/images/working_out.jpg',
+        'content': 'Exercise is a must for a healthy and long life. For those with asthma however, exercise carries many challenges, many of which are fortified by myths such as'
+
+    }, {
+        'name': 'Keep Respiratory Infections At Bay During Winters With These 5 Foods',
+        'link': 'http://www.airveda.com/blog/5-Foods-To-Keep-Respiratory-Infections-At-Bay-During-Winters',
+        'mobile_link': '',
+        'date': 'December 20, 2017',
+        'author': 'Ranjani Raman',
+        'author_photo': '',
+        'about_author': '',
+        'keywords': 'respiratory wellness,breathe well,lifestyle changes,air quality',
+        'image': 'http://www.airveda.com/resources/images/foods_for_infections.jpg',
+        'content': 'Our respiratory system is the most crucial, yet most abused organ system in our body. Various external stressful factors such as polluted air, weather changes, unhealthy food intake, erratic schedules, improper lifestyle etc. play havoc on our immunity, and the'
+
+    }, {
+        'name': 'Health Impact of Particulate Matter PM2.5 and PM10',
+        'link': 'http://www.airveda.com/blog/Health-Impact-of-Particulate-Matter-PM25-and-PM10',
+        'mobile_link': '',
+        'date': 'December 6, 2017',
+        'author': 'Team Airveda',
+        'author_photo': 'http://www.airveda.com/resources/images/team_airveda.jpg',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': "air quality monitor, PM2.5, air pollution, delhi, air quality in india",
+        'image': 'http://www.airveda.com/resources/images/health_impact.jpg',
+        'content': 'Every day we come across headlines screaming in our face on the imminent dangers of the polluted air engulfing our surroundings. We could probably go three weeks without food, three days without water, but it is difficult to go beyond three minutes without air'
+
+    }, {
+        'name': 'How to Stay Active During Winters In Spite of Pollution',
+        'link': 'http://www.airveda.com/blog/How-to-Stay-Active-During-Winters-In-Spite-of-Pollution',
+        'mobile_link': '',
+        'date': 'November 20, 2017',
+        'author': 'Team Airveda',
+        'author_photo': '',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'PM2.5, exercising, air pollution, air pollution in india, air quality monitor, airveda, breathing in winter, respiratory wellness',
+        'image': 'http://www.airveda.com/resources/images/stay_active.jpg',
+        'content': 'There is no better start to a winter day than a run in the crisp morning air, followed by breakfast while basking in the warm winter sun. Increasingly, however, winters have become an omen of high pollution levels, severe air quality, choking smog and endless respiratory problems.'
+
+    }, {
+        'name': 'Eat Right to Breathe Right!',
+        'link': 'http://www.airveda.com/blog/foods-for-respiratory-wellness',
+        'mobile_link': '',
+        'date': 'November 8, 2017',
+        'author': 'Saroj Modi',
+        'author_photo': 'http://www.airveda.com/resources/images/saroj_modi.jpg',
+        'about_author': 'Saroj Modi is the Founder of <a href="http://theinnerstartup.com/">The Inner Startup promoting</a> an exploration of our world inside-out to energize our lives.',
+        'keywords': 'breathing problems, diet, the inner startup, saroj modi, immune system',
+        'image': 'http://www.airveda.com/resources/images/eat_right.jpg',
+        'content': 'Ayurveda - one of the oldest bodies of knowledge on holistic wellness, has its roots ingrained in the philosophy that food plays a big part in our well-being. As per Ayurveda and Yoga humans have psycho-physiological constitution.'
+
+    }, {
+        'name': 'Measuring Data - Understanding Particulate Matter and How It Impacts Our Health',
+        'link': 'http://www.airveda.com/blog/Understanding-Particulate-Matter-and-Its-Associated-Health-Impact',
+        'mobile_link': '',
+        'date': 'October 22, 2017',
+        'author': 'Team Airveda',
+        'author_photo': '',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'Air pollution,air quality monitor,air pollution control,PM2.5,pollution masks,anti pollution masks,air quality monitor,air purifier,airveda,iBreathe,Air quality,PM 2.5,PM 10,particulate matter,air cleaner',
+        'image': 'http://www.airveda.com/resources/images/understanding_data_blog.jpg',
+        'content': 'There has been significant research around the effects of PM2.5 and PM10 worldwide which has connected high levels of exposure to these pollutants with significant health problems. Here is how Airveda is helping monitor that.'
+
+    }, {
+        'name': 'Fun Ways To Celebrate a Firecracker-free Diwali',
+        'link': 'http://www.airveda.com/blog/Fun-Ways-To-Celebrate-a-Firecracker-free-Diwali',
+        'mobile_link': '',
+        'date': 'October 17, 2017',
+        'author': 'Namita Gupta',
+        'author_photo': 'http://www.airveda.com/resources/images/namita_author.jpg',
+        'about_author': 'Namita Gupta is the founder of Airveda, which is helping people control the air they breathe and create a citizen\'s movement against air pollution. An IIT Delhi alumnus, she has worked many years in the US at Microsoft, and Facebook, and was CPO at Zomato after she returned to India. She is also a proud mom to two little girls.',
+        'keywords': 'diwali,green diwali,pollution during diwali,air pollution,delhi air quality,air pollution in delhi,breathe better',
+        'image': 'http://www.airveda.com/resources/images/diwali.jpg',
+        'content': 'Growing up, Diwali signified new beginnings, fun and festivities. The house bustled with friends and family and lights and smells that were truly unique to the time of year. As children, it meant new clothes, gifts, decorations, rangolis and'
+
+    }, {
+        'name': '6 Things You Need To Know When Buying An Air Purifier [STUDY]',
+        'link': 'http://airveda.com/blog/6-Things-You-Need-To-Know-When-Buying-An-Air-Purifier-STUDY',
+        'mobile_link': '',
+        'date': 'October 5, 2017',
+        'author': 'Team Airveda',
+        'author_photo': '',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'asthma,air purifiers,air filtration machine,ozone,hepa,CADR,indoor air quality,delhi air quality,air quality india',
+        'image': 'http://airveda.com/resources/images/purifier_study.jpeg',
+        'content': 'Many people have asked us our views on which purifiers are most effective. So we decided to use the Airveda monitor to do a small study on 3 popular Air Purifiers'
+
+    }, {
+        'name': 'Simple Remedies To Help You Counter Nasal Congestion',
+        'link': 'http://airveda.com/blog/simple-remedies-to-help-you-counter-nasal-congestion',
+        'mobile_link': '',
+        'date': 'September 28, 2017',
+        'author': 'Saroj Modi',
+        'author_photo': 'http://www.airveda.com/resources/images/saroj_modi.jpg',
+        'about_author': 'Saroj Modi is the Founder of <a href="http://theinnerstartup.com/">The Inner Startup promoting</a> an exploration of our world inside-out to energize our lives.',
+        'keywords': 'nasal congestion,stuffy nose,nasal polyps,home remedies,air pollution,respiratory health,healthy living,yoga,pranayam,breathing problems',
+        'image': 'http://airveda.com/resources/images/remedies.png',
+        'content': 'Nasal congestion or stuffy nose is the blockage of the nasal passages usually due to membranes, lining the nose, becoming swollen from inflamed pm2blood vessels. Typical symptoms of nasal congestion are nasal discharge, congestion, cough, fever'
+    }, {
+        'name': '4 Easy Lifestyle Changes To Reduce Your Carbon Footprint',
+        'link': 'http://airveda.com/blog/easy-lifestyle-changes-to-reduce-your-carbon-footprint',
+        'mobile_link': '',
+        'date': 'September 27, 2017',
+        'author': 'Team Airveda',
+        'author_photo': 'http://www.airveda.com/resources/images/team_airveda.jpg',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'Carbon footprint,Air pollution,Local farming,Energy efficiency,Fossil fuel,Green living, air pollution control, PM2.5',
+        'image': 'http://airveda.com/resources/images/carbon.png',
+        'content': 'Air pollution and global warming are now a major threat to all mankind. WHO reports that around 7 million people die in a year as a result of air pollution exposure. 65% of the deaths in Asia and 25% deaths in India are due to air pollution alone.'
+    }, {
+        'name': 'How to Choose the Right Anti-Pollution Mask',
+        'link': 'http://airveda.com/blog/how-to-choose-the-right-anti-pollution-mask',
+        'mobile_link': '',
+        'date': 'September 26, 2017',
+        'author': 'Team Airveda',
+        'author_photo': 'http://www.airveda.com/resources/images/team_airveda.jpg',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'Air pollution,Green living,air pollution control,PM2.5, pollution masks,anti pollution masks,air quality monitor,purifier,airveda,iBreathe,Air quality,PM 2.5,PM 10,particulate matter',
+        'image': 'http://airveda.com/resources/images/right_mask.png',
+        'content': 'Air pollutants are emitted by a number of sources  industries, households, farming activities, vehicles, and even natural occurrences such as forest fires, dust storms and volcanic activity. '
+    }, {
+        'name': 'What Is PM2.5 and Why Is It Important?',
+        'link': 'http://airveda.com/blog/what-is-pm2-5-and-why-is-it-important',
+        'mobile_link': '',
+        'date': 'September 25, 2017',
+        'author': 'Team Airveda',
+        'author_photo': 'http://www.airveda.com/resources/images/team_airveda.jpg',
+        'about_author': 'Airveda team is a group of people helping everyone breathe well and live well.',
+        'keywords': 'Air pollution,air quality monitor, air pollution control, PM2.5, pollution masks, anti pollution masks, air quality monitor, air purifier,airveda, iBreathe, Air quality,PM 2.5,PM 10,particulate matter, air cleaner',
+        'image': 'http://airveda.com/resources/images/pm.png',
+        'content': 'We know when food is dirty or water is impure but when it comes to air we do not realize how polluted it is because we cannot see it. And we breathe in almost 3000 gallons of air every day. While larger pollutants are directly visible to the eye.'
+    }, {
+        'name': 'Birth of Airveda',
+        'link': 'http://airveda.com/blog/birth-of-airveda',
+        'mobile_link': '',
+        'date': 'September 24, 2017',
+        'author': 'Namita Gupta',
+        'author_photo': 'http://www.airveda.com/resources/images/namita_author.jpg',
+        'about_author': 'Namita Gupta is the founder of Airveda, which is helping people control the air they breathe and create a citizen\'s movement against air pollution. An IIT Delhi alumnus, she has worked many years in the US at Microsoft, and Facebook, and was CPO at Zomato after she returned to India. She is also a proud mom to two little girls.',
+        'keywords': 'Namita Gupta,Air pollution,air quality monitor,air pollution control,PM2.5,pollution masks,anti pollution masks,air quality monitor,air purifier,airveda,iBreathe,Air quality,PM 2.5,PM 10,particulate matter, air cleaner',
+        'image': 'http://airveda.com/resources/images/birth.jpg',
+        'content': 'Asthma runs in my family. My dad, myself and my daughter are all asthmatic. After being in the US for 13 years, we moved to India in Dec 2013. In 2014 Delhi was declared the most polluted city in the world.'
+    }
+]
